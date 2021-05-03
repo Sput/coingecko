b3c23917563e76aceedd30a87a7f19e3966b55cb
Sput <paulknick@gmail.com>
2021-05-02 21:00:55 -0400

    API call added, output to console

---
+++ app.js
@@ -0,0 +1,24 @@
+fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+.then(data=>{return data.json()})
+.then(chart=>{console.log(chart)})
+
+
+
+
+/*
+dbParam = JSON.stringify(chart);
+xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    myObj = JSON.parse(this.responseText);
+    txt += "<table border='1'>"
+    for (x in myObj) {
+      txt += "<tr><td>" + myObj[x].name + "</td></tr>";
+    }
+    txt += "</table>"
+    document.getElementById("demo").innerHTML = txt;
+  }
+}
+xmlhttp.open("POST", "json_demo_db_post.php", true);
+xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+xmlhttp.send("x=" + dbParam); */
